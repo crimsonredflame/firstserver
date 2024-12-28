5268f617a2906378b11f39ce285c9d1aac9a47fe
crimsonredflame <tanisha7565@gmail.com>
2024-12-29 01:13:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def read_items():
+    return """
+    <html>
+        <head>
+            <title>Ye hai mera tabname</title>
+        </head>
+        <body>
+            <h3 style="{color: "pink"}">Balle balle de shava shava piyo piyo te kava kava!</h1>
+        </body>
+    </html>
+    """
 
 @app.get("/hiakku")
 async def root():
